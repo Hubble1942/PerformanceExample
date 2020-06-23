72283a3b33a82965fe6e3651959006abcf72dd51
Christian Ewald <christian.ewald@m-f.ch>
2020-06-23 12:08:57 +0200

    Add some documentation

--- PerformanceExample/Box.cs
+++ PerformanceExample/Box.cs
@@ -7,14 +7,31 @@ namespace PerformanceExample
     /// </summary>
     public struct Box
     {
+        /// <summary>
+        /// Returns true if this box is valid.
+        /// </summary>
         public bool IsValid { get; private set; }
 
+        /// <summary>
+        /// Gets the minimum corner point.
+        /// </summary>
         public Point3D Min { get; private set; }
 
+        /// <summary>
+        /// Gets the maximum corner point.
+        /// </summary>
         public Point3D Max { get; private set; }
 
+        /// <summary>
+        /// Gets the center point.
+        /// </summary>
         public Point3D Center => (Point3D)(((Vector3D)this.Min + (Vector3D)this.Max) / 2.0);
 
+        /// <summary>
+        /// Creates an extended box enclosing both, this instance and the specified point.
+        /// </summary>
+        /// <param name="point">The point.</param>
+        /// <returns>The extended box.</returns>
         public Box Enclose(Point3D point)
         {
             if (this.IsValid)
@@ -43,6 +60,13 @@ namespace PerformanceExample
             }
         }
 
+        /// <summary>
+        /// Creates an extended box enclosing both, this instance and the specified other box.
+        /// </summary>
+        /// <param name="other">The other box.</param>
+        /// <returns>
+        /// The extended box.
+        /// </returns>
         public Box Enclose(Box other)
         {
             if (other.IsValid)
