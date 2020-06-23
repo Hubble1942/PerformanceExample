fbb737ac53d5b7754bdc7395834bfa8037b3db52
Christian Ewald <christian.ewald@m-f.ch>
2020-06-23 09:04:28 +0200

    Add initial version

---
+++ PerformanceExample/BoundingBoxCalculator.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace PerformanceExample
+{
+    internal static class BoundingBoxCalculator
+    {
+        // Lock for the multithreaded loop.
+        private static object theLock = new object();
+
+        /// <summary>
+        /// Original code.
+        /// </summary>
+        public static Point3D CalculateOriginal(IEnumerable<IEnumerable<Point3D>> polylines)
+        {
+            var minValues = new Point3D(double.MaxValue, double.MaxValue, double.MaxValue);
+            var maxValues = new Point3D(double.MinValue, double.MinValue, double.MinValue);
+
+            foreach (var line in polylines)
+            {
+                foreach (var point in line)
+                {
+                    minValues.X = Math.Min(minValues.X, point.X);
+                    minValues.Y = Math.Min(minValues.Y, point.Y);
+                    minValues.Z = Math.Min(minValues.Z, point.Z);
+
+                    maxValues.X = Math.Max(maxValues.X, point.X);
+                    maxValues.Y = Math.Max(maxValues.Y, point.Y);
+                    maxValues.Z = Math.Max(maxValues.Z, point.Z);
+                }
+            }
+
+            return (Point3D)(((Vector3D)maxValues + (Vector3D)minValues) / 2.0);
+        }
+
+        /// <summary>
+        /// Performance optimized to allow fluent, interactive positioning.
+        /// Release mode: 1749 ms => 113 ms.
+        /// </summary>
+        /// <remarks>
+        /// This remark has just been added, because this example is used in a blog. In practice, 
+        /// I would just add the summary above.
+        /// - Replaced IEnumerable with List: 1749 ms => 1229 ms.
+        /// - Replaced Math.Min and Math.Max with "if smaller/larger => assign". 1229 ms => 984 ms.
+        /// - Use double instead of Point3D for intermediate results. 984 ms => 934 ms.
+        /// - Use for loops instead of forech loops. 934 ms => 334 ms.
+        /// - Use Parallel.For instead of for. 334 ms => 113 ms. With 4 (8) cores, ProcessorCount = 8. 
+        ///   When all thread pool threads are in use (sleeping), without SetMinThreads => 423 ms (!).
+        /// </remarks>
+        public static Point3D CalculatePerformanceOptimized(List<List<Point3D>> polylines)
+        {
+            ThreadPool.GetMinThreads(out int workerThreads, out int completionPortThreads);
+            int sufficientWorkerThreads = workerThreads + 2 * Environment.ProcessorCount;
+            ThreadPool.SetMinThreads(sufficientWorkerThreads, completionPortThreads);
+
+            double minX = double.MaxValue;
+            double minY = double.MaxValue;
+            double minZ = double.MaxValue;
+            double maxX = double.MinValue;
+            double maxY = double.MinValue;
+            double maxZ = double.MinValue;
+
+            Parallel.For(0, polylines.Count, polylineIndex =>
+            {
+                double innerMinX = double.MaxValue;
+                double innerMinY = double.MaxValue;
+                double innerMinZ = double.MaxValue;
+                double innerMaxX = double.MinValue;
+                double innerMaxY = double.MinValue;
+                double innerMaxZ = double.MinValue;
+
+                List<Point3D> line = polylines[polylineIndex];
+                for (int pointIndex = 0; pointIndex < line.Count; pointIndex++)
+                {
+                    Point3D point = line[pointIndex];
+                    if (point.X < innerMinX) { innerMinX = point.X; }
+                    if (point.Y < innerMinY) { innerMinY = point.Y; }
+                    if (point.Z < innerMinZ) { innerMinZ = point.Z; }
+
+                    if (point.X > innerMaxX) { innerMaxX = point.X; }
+                    if (point.Y > innerMaxY) { innerMaxY = point.Y; }
+                    if (point.Z > innerMaxZ) { innerMaxZ = point.Z; }
+                }
+
+                lock (theLock)
+                {
+                    if (innerMinX < minX) { minX = innerMinX; }
+                    if (innerMinY < minY) { minY = innerMinY; }
+                    if (innerMinZ < minZ) { minZ = innerMinZ; }
+
+                    if (innerMaxX > maxX) { maxX = innerMaxX; }
+                    if (innerMaxY > maxY) { maxY = innerMaxY; }
+                    if (innerMaxZ > maxZ) { maxZ = innerMaxZ; }
+                }
+            });
+
+            ThreadPool.SetMinThreads(workerThreads, completionPortThreads);
+
+            return new Point3D(minX + maxX / 2.0, minY + maxY / 2.0, minZ + maxZ / 2.0);
+        }
+    }
+}
---
+++ PerformanceExample/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Media.Media3D;
+
+namespace PerformanceExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Program started.");
+
+            List<List<Point3D>> inputData = CreateInputData();
+
+            var stopwatch = new Stopwatch();
+            Point3D result;
+            long usedMilliseconds;
+
+            ThreadPool.QueueUserWorkItem(SleepABit, new Object());
+            ThreadPool.QueueUserWorkItem(SleepABit, new Object());
+            ThreadPool.QueueUserWorkItem(SleepABit, new Object());
+            ThreadPool.QueueUserWorkItem(SleepABit, new Object());
+            ThreadPool.QueueUserWorkItem(SleepABit, new Object());
+            ThreadPool.QueueUserWorkItem(SleepABit, new Object());
+            ThreadPool.QueueUserWorkItem(SleepABit, new Object());
+            ThreadPool.QueueUserWorkItem(SleepABit, new Object());
+
+            ThreadPool.QueueUserWorkItem(SleepABit, new Object());
+            ThreadPool.QueueUserWorkItem(SleepABit, new Object());
+            ThreadPool.QueueUserWorkItem(SleepABit, new Object());
+            ThreadPool.QueueUserWorkItem(SleepABit, new Object());
+            ThreadPool.QueueUserWorkItem(SleepABit, new Object());
+            ThreadPool.QueueUserWorkItem(SleepABit, new Object());
+            ThreadPool.QueueUserWorkItem(SleepABit, new Object());
+            ThreadPool.QueueUserWorkItem(SleepABit, new Object());
+
+            stopwatch.Restart();
+            result = BoundingBoxCalculator.CalculateOriginal(inputData);
+            usedMilliseconds = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine($"Cache warmed in {usedMilliseconds} milliseconds. Result: ({result}).");
+
+            stopwatch.Restart();
+            result = BoundingBoxCalculator.CalculateOriginal(inputData);
+            usedMilliseconds = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine($"Original calculation of {result} used {usedMilliseconds} milliseconds.");
+            stopwatch.Restart();
+            result = BoundingBoxCalculator.CalculateOriginal(inputData);
+            usedMilliseconds = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine($"Original calculation of {result} used {usedMilliseconds} milliseconds.");
+            stopwatch.Restart();
+            result = BoundingBoxCalculator.CalculateOriginal(inputData);
+            usedMilliseconds = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine($"Original calculation of {result} used {usedMilliseconds} milliseconds.");
+
+            stopwatch.Restart();
+            result = BoundingBoxCalculator.CalculatePerformanceOptimized(inputData);
+            usedMilliseconds = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine($"Cache warmed in {usedMilliseconds} milliseconds. Result: ({result}).");
+
+            stopwatch.Restart();
+            result = BoundingBoxCalculator.CalculatePerformanceOptimized(inputData);
+            usedMilliseconds = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine($"Optimized calculation of {result} used {usedMilliseconds} milliseconds.");
+            stopwatch.Restart();
+            result = BoundingBoxCalculator.CalculatePerformanceOptimized(inputData);
+            usedMilliseconds = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine($"Optimized calculation of {result} used {usedMilliseconds} milliseconds.");
+            stopwatch.Restart();
+            result = BoundingBoxCalculator.CalculatePerformanceOptimized(inputData);
+            usedMilliseconds = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine($"Optimized calculation of {result} used {usedMilliseconds} milliseconds.");
+
+            Console.WriteLine("Program finished.");
+            Console.ReadKey();
+        }
+
+        private static List<List<Point3D>> CreateInputData()
+        {
+            const int numberOfPolylines = 50000;
+            const int numberOfPoints = 1000;
+
+            List<List<Point3D>> returnValue = new List<List<Point3D>>();
+
+            for (int iPolyline = 0; iPolyline < numberOfPolylines; iPolyline++)
+            {
+                var newPolyline = new List<Point3D>();
+                
+                for (int iPoint = 0; iPoint < numberOfPoints; iPoint++)
+                {
+                    newPolyline.Add(new Point3D(iPolyline + iPoint, iPolyline + iPoint, iPolyline + iPoint));
+                }
+
+                returnValue.Add(newPolyline);
+            }
+
+            return returnValue;
+        }
+
+        private static void SleepABit(Object theObject)
+        {
+            Thread.Sleep(10000);
+        }
+    }
+}
